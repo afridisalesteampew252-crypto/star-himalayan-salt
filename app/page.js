24374e651824fe3f5c3ef24a202143584caa3275
afridisalesteampew252-crypto <afridisalesteampew252@gmail.com>
2026-04-18 17:25:30 +0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main style={{padding:20, textAlign:"center"}}>
+      <h1>STAR Himalayan Pink Salt</h1>
+      <p>Premium Export Quality Salt from Pakistan</p>
+
+      <a href="https://wa.me/923318484115">
+        WhatsApp Us
+      </a>
+
+      <br /><br />
+
+      <a href="/products">View Products</a>
+    </main>
+  );
+}
